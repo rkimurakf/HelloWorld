3b0e15e8c89ee975e025a18f7d3304cd7786e857
ryuuto.kimura <r_kimura@kyoei-e.com>
2024-09-17 15:59:09 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by mba2408.silver kyoei.engine on 2024/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
